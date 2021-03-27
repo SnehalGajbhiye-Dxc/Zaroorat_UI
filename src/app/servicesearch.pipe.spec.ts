a52a403e52b8b75efe6bb7b939e106a6cd28556b
spathade <shweta.pathade@dxc.com>
2021-03-27 16:40:51 +0530

    filter added

---
+++ src/app/servicesearch.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ServicesearchPipe } from './servicesearch.pipe';
+
+describe('ServicesearchPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ServicesearchPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
